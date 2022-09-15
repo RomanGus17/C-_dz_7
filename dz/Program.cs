dc9e9fcd11fd4a0a95592870558acac5893f247a
Roman <romangushchintlt@gmail.com>
2022-09-15 20:11:07 +0400

    выполнил задачу 50

--- dz/Program.cs
+++ dz/Program.cs
@@ -37,3 +37,54 @@
 // double[,] numbers = new double[QuantityLines, QuantityColumns];
 // FillArrayRandomRealNumbers(numbers);
 // PrintArray(numbers);
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+// Console.WriteLine("Введите строку позиции элемента");
+// int QuantityLines = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите колоноку позиции элемента");
+// int QuantityColumns = Convert.ToInt32(Console.ReadLine());
+
+// void FillArrayRandomRealNumbersInt(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArrayInt(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine("");
+//     }
+// }
+
+
+// int [,] numbers = new int [10,10];
+// FillArrayRandomRealNumbersInt(numbers);
+
+// if (QuantityLines > numbers.GetLength(0) || QuantityColumns > numbers.GetLength(1))
+// {
+//     Console.WriteLine("такого числа в массиве нет");
+// }
+// else
+// {
+//     Console.WriteLine($"значение элемента = {numbers[QuantityLines-1,QuantityColumns-1]}");
+// }
+
+// PrintArrayInt(numbers);
